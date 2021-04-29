80b941cad151ebcc503cd476e6d5a68c9ec8ff68
arun-natarajan-99 <80808104+arun-natarajan-99@users.noreply.github.com>
2021-04-29 23:45:59 +0530

    Add files via upload

---
+++ Activity_1/main.c
@@ -0,0 +1,39 @@
+/**
+ * @file project_main.c
+ * @author Arun Natarajan (arun291999@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-04-20
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <avr/io.h>
+#include <util/delay.h> // Delay Library
+
+
+int main(void)
+{
+
+port();
+    while(1)
+    {
+        if(!(PIND&(1<<PD0))) // switch press
+        {
+         PORTB|=(1<<PB0); // LED ON
+         _delay_ms(2000); // Delay 2000ms
+        }
+        else
+        {
+          PORTB&=~(1<<PB0); //LED Off
+         _delay_ms(2000); //Delay 2000ms
+        }
+       /* PORTB|=(1<<PB0);
+        _delay_ms(2000);
+        PORTB&=~(1<<PB0);
+        _delay_ms(2000); */
+    }
+
+    return 0;
+}
