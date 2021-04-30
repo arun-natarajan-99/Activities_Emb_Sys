4157ee36e1a06e6267e44ad75f1e12bc105501ae
arun-natarajan-99 <80808104+arun-natarajan-99@users.noreply.github.com>
2021-04-30 20:28:25 +0530

    Add files via upload

---
+++ Activity_4/main.c
@@ -0,0 +1,30 @@
+/*
+ */
+
+#include <avr/io.h>
+#include<util/delay.h>
+#include "uart.h"
+
+int main(void)
+{
+    char data;
+    DDRB|=(1<<PB0);
+    USARTInit(103);
+
+
+
+    while(1)
+    {
+      data=USARTReadChar();
+       // USARTWriteChar('A');
+        if(data=='A')
+        {
+            PORTB|=(1<<PB0);
+            _delay_ms(200);
+            PORTB&=~(1<<PB0);
+            _delay_ms(200);
+        }
+    }
+
+    return 0;
+}
